1142cff1ffd8d77bcf4be0c1ad7172849737a627
sabricakir <sabricakir86@gmail.com>
2022-07-10 19:05:56 +0300

    switched errors language to turkish

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,10 @@ class User < ApplicationRecord
 
    has_one_attached :avatar
 
-   validates :name, :surname , presence:  true, length: {in: 2..25 , too_long: "%{count} characters is the maximum allowed" }
-   validates :phone , presence: true, length: {is:10}, uniqueness: true, numericality: true
-   validates :schoolNumber , uniqueness: true , allow_nil: true, length:  {maximum: 8}, numericality: true
+   validates :name, presence: {message:"İsim alanı zorunludur"}, length: {in: 2..20 , too_long: "İsim en fazla %{count} karakterden oluşabilir", too_short: "İsim alanı en az 2 karakterden oluşmalıdır" }
+   validates :surname, presence:{message:"Soyisim alanı zorunludur"}, length: {in: 2..25 , too_long: "Soyisim en fazla %{count} karakterden oluşabilir", too_short: "Soyisim alanı en az 2 karakterden oluşmalıdır" }
+   validates :phone , presence: {message:"Telefon alanı zorunludur"}, length: {is:10,message:"Telefon 10 haneden oluşmalıdır"}, uniqueness:{message:"Bu telefon numarası zaten kayıtlı"}, numericality:{message:"Telefon sadece rakamlardan oluşabilir"}
+   validates :schoolNumber , uniqueness:{message:"Bu okul numarası zaten kayıtlı"} , allow_nil: true, length:  {maximum: 8, message:"Okul numarası en fazla 8 haneden oluşmalıdır"}, numericality:{message:"Okul numarası sadece rakamlardan oluşabilir"}
 
 
 
